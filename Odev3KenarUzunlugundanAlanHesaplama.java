56cf6cc48fb88b576d9a3590d3b51d5df9ab8704
Gamze Aslan <95714545+gamzeaslan@users.noreply.github.com>
2022-04-16 16:57:28 +0300

    Update Odev3KenarUzunlugundanAlanHesaplama.java

--- Odev3KenarUzunlugundanAlanHesaplama.java
+++ Odev3KenarUzunlugundanAlanHesaplama.java
@@ -10,11 +10,11 @@ public class Odev3KenarUzunlugundanAlanHesaplama {
         double kenar2=sc.nextDouble();
         System.out.print("3.Kenar: ");
         double kenar3=sc.nextDouble();
-        
+        // üçgenin çevresinin yarısını hesaplama
         double u=(kenar1+kenar2+kenar3)/2;
-        
+        //üçgenin alanının karesini hesaplama
         double ucgenAlan=u*(u-kenar1)*(u-kenar2)*(u-kenar3);
-        
+        // yukarıdaki değerin karekökünü alıp çıktı olarak verme
         System.out.println(kenar1+"-"+kenar2+"-"+kenar3+" kenarlarına sahip üçgenin alanı= "+Math.sqrt(ucgenAlan));
         
     }
