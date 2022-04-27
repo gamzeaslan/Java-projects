54d502e5fb7a86b472323a56a5f7a940597d4937
Gamze Aslan <95714545+gamzeaslan@users.noreply.github.com>
2022-04-28 01:04:22 +0300

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+
+package fibonacci; 
+import java.util.Scanner ;
+public class Fibonacci{
+    public static void main(String[] args) {
+     int adedi;
+        int sayi1=1;
+        int sayi2=2;
+        Scanner klavye =new Scanner (System.in);
+        System.out.print("bir tamsayı değeri giriniz= ");
+        adedi=klavye.nextInt();
+        System.out.println("0");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("2");
+        for( int i=1;i<=adedi-4;i++){
+            int toplam =sayi1+sayi2;
+            System.out.println(toplam);
+            sayi1=sayi2;
+            sayi2=toplam;
+        }
+    }
+    
+}
