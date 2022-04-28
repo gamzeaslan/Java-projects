2d2ef0b93429996ae19924f16d2f216e2960e699
Gamze Aslan <95714545+gamzeaslan@users.noreply.github.com>
2022-04-28 11:18:50 +0300

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -5,15 +5,13 @@ public class Fibonacci{
     public static void main(String[] args) {
      int adedi;
         int sayi1=1;
-        int sayi2=2;
+        int sayi2=1;
         Scanner klavye =new Scanner (System.in);
         System.out.print("bir tamsayı değeri giriniz= ");
         adedi=klavye.nextInt();
-        System.out.println("0");
         System.out.println("1");
         System.out.println("1");
-        System.out.println("2");
-        for( int i=1;i<=adedi-4;i++){
+        for( int i=1;i<=adedi-2;i++){
             int toplam =sayi1+sayi2;
             System.out.println(toplam);
             sayi1=sayi2;
